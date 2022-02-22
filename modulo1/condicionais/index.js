70a6f04d1555d0cf949c598516ef2c2e5d2de7b9
Frederico-Martins <Fredlsmartins@outlook.com>
2022-02-22 15:03:52 -0300

    condicionais

---
+++ modulo1/condicionais/index.js
@@ -0,0 +1,82 @@
+//Exercicio de interpretação
+//1)
+//A) Esse codigo esta falando que se o numero for par quer dizer que "passou no teste"
+//Se for impa, else vai falar que' não passou no teste'.
+
+//B)Para numeros pares
+
+//C)Para numeros impares
+
+//2)
+//A)O codigo a cima serve para automatizar tarefas do supermercado
+
+//B)vai ser impresso no console o preco da "Maça, no caso preco = 2,25"
+
+//C)A mensagem impressa se ele fosse comprar uma pera seria "preço = 5.5".
+//Se tirasse o break o valor de pera seria o de baixo "preço = 5"
+
+//3)
+//A)A primeira linha esta falando para o usuario digitar um numero.
+
+
+//B)Vai aparecer esse numero passou no teste
+//No caso se fosse - 10 não iria aparecer nada
+//Iria dar erro pois a varivael mensagem não foi definida
+
+//C)Iria da erro por conta que a impressão da ''mensagem'' não foi imprimida dentro da funçao
+
+//Exercicios de escrita de código
+//Exercicio 1
+
+// let idade  = Number (prompt ("Qual a sua idade?"))
+// const maiorDeIdade = 18 
+// if (idade>= maiorDeIdade) {
+// console.log ("Você pode dirigir")
+// }else{
+// console.log ("Você não pode dirigir")  
+// }
+
+//Exercicio2
+// let pergunta = prompt("digite M (matutino) ou V (Vespertino) ou N (Noturno). ")
+// let turno = {
+//     matutino: "M",
+//     vespertino: "V",
+//     noturno: "N"}
+// if (pergunta  === turno.matutino) {
+//     console.log("Bom dia!!!")
+// }else if (pergunta === turno.vespertino) {
+//     console.log("Boa tarde!!")
+// }else if(pergunta === turno.noturno) {
+//     console.log("Boa noite!!")
+// }else {
+//     console.log ("Opção invalida")
+// }
+
+//Exercicio3
+// let turno = {
+//     matutino: "M",
+//     vespertino: "V",
+//     noturno: "N"}
+// switch(prompt("digite M (matutino) ou V (Vespertino) ou N (Noturno). ")){
+ 
+// case "M" :
+//     console.log("Bom dia!!!")
+//     break;
+//  case "V":
+//     console.log("Boa tarde!!")
+//     break;
+// case "N":
+//     console.log("Boa noite!!")
+// break;
+// default:
+//     console.log ("Opção invalida")
+// }
+
+//Exercicio 4
+// let  pergunta = prompt ("Qual o gênero do filme vão asssitr ?")
+// let preco = Number(prompt ("Qual valor do ingresso"))
+// if (pergunta === "fantasia" && preco  < 15){
+// console.log ("Bom filme!!")
+// }else{
+// console.log ('Escolha outro filme!!')
+// }
